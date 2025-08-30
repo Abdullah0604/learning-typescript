cc226b97f3b843d406a4d34e6dcf6d838db21434
greenFruits <mdabdullah6548ctg@gmail.com>
2025-08-30 22:29:01 +0600

    learned basic types in lesson 01

---
+++ lesson-01/app.js
@@ -0,0 +1,11 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 20;
+var n2 = 30;
+console.log(add(n1, n2));
+function multiplyTwoNumber(a, b) {
+    var sum = a * b;
+    return sum;
+}
+// console.log(n1, n2);
---
+++ lesson-01/app.ts
@@ -0,0 +1,14 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const n1 = 20;
+const n2 = 30;
+
+console.log(add(n1, n2));
+
+function multiplyTwoNumber(a, b) {
+  const sum = a * b;
+  return sum;
+}
+// console.log(n1, n2);
