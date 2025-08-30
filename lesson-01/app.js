393d76e04cf21108c36f6959d5843cae82978225
greenFruits <mdabdullah6548ctg@gmail.com>
2025-08-30 22:45:17 +0600

    Learned the basic type of typescript in lesson one

--- lesson-01/app.js
+++ lesson-01/app.js
@@ -1,11 +1,13 @@
-function add(num1, num2) {
-    return num1 + num2;
+// basic type: number, string, boolean
+function add(num1, num2, text, isPrint) {
+    var sum = num1 + num2;
+    if (isPrint) {
+        return "".concat(text, " ").concat(sum);
+    }
+    return "You don't want to print";
 }
 var n1 = 20;
 var n2 = 30;
-console.log(add(n1, n2));
-function multiplyTwoNumber(a, b) {
-    var sum = a * b;
-    return sum;
-}
+var someText = "Two number of sum is: ";
+console.log(add(n1, n2, someText, false));
 // console.log(n1, n2);
--- lesson-01/app.ts
+++ lesson-01/app.ts
@@ -1,14 +1,18 @@
-function add(num1: number, num2: number) {
-  return num1 + num2;
+// basic type: number, string, boolean
+
+function add(num1: number, num2: number, text: string, isPrint: boolean) {
+  const sum = num1 + num2;
+  if (isPrint) {
+    return `${text} ${sum}`;
+  }
+
+  return "You don't want to print";
 }
 
 const n1 = 20;
 const n2 = 30;
+const someText = "Two number of sum is: ";
 
-console.log(add(n1, n2));
+console.log(add(n1, n2, someText, false));
 
-function multiplyTwoNumber(a, b) {
-  const sum = a * b;
-  return sum;
-}
 // console.log(n1, n2);
